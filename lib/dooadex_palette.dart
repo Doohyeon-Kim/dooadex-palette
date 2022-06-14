3f5253fce0afb121eebc2eb0e9e1124bec64d660
doohyeon.kim <doohyeon.kim@doohyeonkims-MacBook-Pro.local>
2022-06-14 23:59:47 +0900

    Edit library file

--- lib/dooadex_palette.dart
+++ lib/dooadex_palette.dart
@@ -6,6 +6,7 @@ export 'package:dooadex_palette/src/color/dooadex_color_swatch.dart';
 
 /// Typography
 export 'package:dooadex_palette/src/typography/dooadex_typo.dart';
+export 'package:dooadex_palette/src/typography/typo_constants.dart';
 
 /// Palette Tool
 export 'package:dooadex_palette/src/palette_tools.dart';
